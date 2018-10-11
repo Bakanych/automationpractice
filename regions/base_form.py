9ad8aa8b688e3d2ccb7921fb780ed67a1b7ebdf9
bakanych <manych@gmail.com>
2018-10-11 18:45:41 +0800

    form tests

--- regions/base_form.py
+++ regions/base_form.py
@@ -1,10 +1,20 @@
+from typing import Dict
+
 from pypom import Region
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class BaseForm(Region):
     _header_loc = (By.CSS_SELECTOR,'h3.page-subheading')
 
+    def _field_input_loc(self, label):
+        return (By.XPATH, ".//div[./label='{}']".format(label))
+
+    def is_field_valid(self, label):
+        group_elem:WebElement = self.find_element(*self._field_input_loc(label))
+        return not 'form-error' in group_elem.get_attribute('class')
+
     @property
-    def title(self):
+    def form_title(self):
         return self.find_element(*self._header_loc).text
--- test/authentication_page_tests.py
+++ test/authentication_page_tests.py
@@ -6,17 +6,31 @@ from pages.auth import AuthPage
 def auth_page(browser)->AuthPage:
     return AuthPage(browser).open()
 
+
 def test_auth_page_title(auth_page):
     assert  'Authentication'.upper() == auth_page.title
 
+
 def test_auth_page_contains_sign_up_form(auth_page):
-    assert 'Create an account'.upper() == auth_page.sign_up_form.title
+    assert 'Create an account'.upper() == auth_page.sign_up_form.form_title
 
 
 def test_auth_page_contains_sign_in_form(auth_page):
-    assert 'Already registered?'.upper() == auth_page.sign_in_form.title
+    assert 'Already registered?'.upper() == auth_page.sign_in_form.form_title
+
 
 def test_auth_page_navigation(auth_page):
     assert auth_page.title.lower() == auth_page.current.lower()
     assert 1 == len(auth_page.nav_items)
     assert  'Return to Home' == auth_page.nav_items[0]
+
+def test_sign_in_form_is_valid(auth_page):
+    form = auth_page.sign_in_form
+    assert True == form.is_field_valid('Email address')
+    assert True == form.is_field_valid('Password')
+
+def test_sign_up_form_is_valid(auth_page):
+    form = auth_page.sign_up_form
+    assert True == form.is_valid
+    assert True == form.is_field_valid('Email address')
+
